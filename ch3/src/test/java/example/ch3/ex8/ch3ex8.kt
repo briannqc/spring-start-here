16f48c59b48b4422fbdf33b31192dd4bf82cebdc
briannqc <brian.ngo313@gmail.com>
2022-01-24 11:20:57 +0800

    Using @Qualifier to qualify the bean for higher visibility

--- ch3/src/test/java/example/ch3/ex8/ch3ex8.kt
+++ ch3/src/test/java/example/ch3/ex8/ch3ex8.kt
@@ -3,6 +3,7 @@ package example.ch3.ex8
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -30,6 +31,26 @@ open class ProjectConfigCase1 {
     }
 }
 
+@Configuration
+open class ProjectConfigCase2 {
+
+    @Bean
+    open fun parrot1(): Parrot {
+        return Parrot("Koko")
+    }
+
+    @Bean
+    open fun parrot2(): Parrot {
+        return Parrot("Messi")
+    }
+
+    @Bean
+    open fun person(@Qualifier("parrot1") parrot: Parrot): Person {
+        return Person(parrot)
+    }
+}
+
+
 class Ch3Ex8Test {
 
     @Test
@@ -45,5 +66,20 @@ class Ch3Ex8Test {
         val person = context.getBean(Person::class.java)
 
         assertThat(person.parrot.name).isEqualTo("Messi")
+    }
+
+    @Test
+    @DisplayName(
+        """
+        GIVEN there are multiple parrot beans in the context
+        AND the parameter is annotated with @Qualifier
+        THEN that bean with qualified name is used
+    """
+    )
+    fun `using @Qualifier`() {
+        val context = AnnotationConfigApplicationContext(ProjectConfigCase2::class.java)
+        val person = context.getBean(Person::class.java)
+
+        assertThat(person.parrot.name).isEqualTo("Koko")
     }
 }
