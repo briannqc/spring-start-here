7abe2f372989450924198a2e42583ce4188c8af5
briannqc <brian.ngo313@gmail.com>
2022-01-29 11:18:33 +0800

    [4.3] Focusing on object responsibilities with stereotype annotations
    Spring offers us the @Service annotation to mark a component that takes
    the responsibility of a service and the @Repository annotation to mark
    a component that implements a repository responsibility. All three
    @Component, @Service, and @Repository are stereotype annotations and
    instruct Spring to create and add an instance of the annotated class
    to its context.
    
    The purpose of different annotations are to provide better visibility
    for any developer reading the class.

---
+++ ch4/src/test/java/example/ch4/ex7/ch4ex7.kt
@@ -0,0 +1,61 @@
+package example.ch4.ex7
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.context.annotation.Configuration
+import org.springframework.stereotype.Component
+import org.springframework.stereotype.Repository
+import org.springframework.stereotype.Service
+
+@Service
+class CommentService(
+    private val commentRepository: CommentRepository,
+    private val commentNotificationProxy: CommentNotificationProxy,
+) {
+
+    fun publishComment(comment: Comment) {
+        this.commentRepository.storeComment(comment)
+        this.commentNotificationProxy.sendComment(comment)
+    }
+}
+
+data class Comment(
+    val author: String,
+    val text: String,
+)
+
+interface CommentRepository {
+    fun storeComment(comment: Comment)
+}
+
+@Repository
+class DBCommentRepository : CommentRepository {
+
+    override fun storeComment(comment: Comment) {
+        println("Storing comment: ${comment.text} by ${comment.author}")
+    }
+}
+
+interface CommentNotificationProxy {
+    fun sendComment(comment: Comment)
+}
+
+@Component
+class EmailCommentNotificationProxy : CommentNotificationProxy {
+
+    override fun sendComment(comment: Comment) {
+        println("Notifying comment via Email: ${comment.text} by ${comment.author}")
+    }
+}
+
+@Configuration
+@ComponentScan(basePackages = ["example.ch4.ex7"])
+open class ProjectConfiguration
+
+fun main() {
+    val context = AnnotationConfigApplicationContext(ProjectConfiguration::class.java)
+    val commentService = context.getBean(CommentService::class.java)
+
+    val comment = Comment("Brian", "I'm reading Spring Start Here, the book is a good refresher")
+    commentService.publishComment(comment)
+}
