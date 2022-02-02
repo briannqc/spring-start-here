711f419cec1c5ab4bcc71ee05d7d7e097ec3f67d
briannqc <brian.ngo313@gmail.com>
2022-02-03 00:10:19 +0800

    [6.2.4] Advice annotations other than most used @Around
    @Around is used the most, it's also the most flexible, but we
    don't necessarily need such flexibility, other simpler annotations
    can be used too: @Before, @AfterReturning, @AfterThrowing and @After.

--- ch6/src/main/java/ch6/ex4/aspect/LoggingAspect.java
+++ ch6/src/main/java/ch6/ex4/aspect/LoggingAspect.java
@@ -1,8 +1,10 @@
 package ch6.ex4.aspect;
 
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
 
 import java.util.logging.Logger;
@@ -19,5 +21,19 @@ public class LoggingAspect {
         Object result = joinPoint.proceed();
         logger.info("AFTER " + joinPoint.getSignature());
         return result;
+    }
+
+    @Before("execution(* ch6.ex4.comment.CommentService.publishComment(..))")
+    public void before() {
+        logger.info("We can also use @Before, with is less flexible than @Around but simpler too");
+    }
+
+    @AfterReturning(
+            value = "execution(* ch6.ex4.comment.CommentService.publishComment(..))",
+            returning = "returnedValue"
+    )
+    public void afterReturning(Object returnedValue) {
+        logger.info("Spring also support @After, @AfterReturning and @AfterThrowing");
+        logger.info("We can capture returned value in @AfterReturning with 'returning' param, it was: " + returnedValue + " here");
     }
 }
