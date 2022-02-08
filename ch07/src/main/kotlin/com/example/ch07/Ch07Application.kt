2138c08a553963776f63f5c81f4b4a214d098459
briannqc <brian.ngo313@gmail.com>
2022-02-09 00:38:04 +0800

    CH7: Init a SpringBoot web app with Spring Initializer

---
+++ ch07/src/main/kotlin/com/example/ch07/Ch07Application.kt
@@ -0,0 +1,11 @@
+package com.example.ch07
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Ch07Application
+
+fun main(args: Array<String>) {
+	runApplication<Ch07Application>(*args)
+}
