d5ddc6c0660f3ff478ed27fdb2f28d8721facd6e
briannqc <brian.ngo313@gmail.com>
2022-02-09 00:52:46 +0800

    CH7: A simple web app with @Controller
    @Controller method provides the view name and data that needs to be rendered in view.
    The dispatcher servlet finds the view (by default, in resources/static) and renders
    it with the data provided by the @Controller. To find the view to render, the dispatcher
    servlet uses a component named view resolver.
    
    The view can be any files, not just html. Displaying the file is handled by client,
    a.k.a. Web Browser

--- ch07/src/main/kotlin/com/example/ch07/Ch07Application.kt
+++ ch07/src/main/kotlin/com/example/ch07/Ch07Application.kt
@@ -2,10 +2,22 @@ package com.example.ch07
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.RequestMapping
 
 @SpringBootApplication
 class Ch07Application
 
 fun main(args: Array<String>) {
 	runApplication<Ch07Application>(*args)
+}
+
+@Controller
+class MainController {
+
+	@RequestMapping("/home")
+	fun home(): String = "home.html"
+
+	@RequestMapping("/test")
+	fun test(): String = "test.txt"
 }
