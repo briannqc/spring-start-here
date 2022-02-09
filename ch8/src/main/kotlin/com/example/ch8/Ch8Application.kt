159b15bba045c7d95f6c15bbe169f16dc4d62e55
briannqc <brian.ngo313@gmail.com>
2022-02-10 00:38:55 +0800

    [CH08] Init a project with starter web and thymeleaf

---
+++ ch8/src/main/kotlin/com/example/ch8/Ch8Application.kt
@@ -0,0 +1,11 @@
+package com.example.ch8
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Ch8Application
+
+fun main(args: Array<String>) {
+	runApplication<Ch8Application>(*args)
+}
