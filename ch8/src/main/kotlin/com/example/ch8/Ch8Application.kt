0d8dd7e348f90897011a67d04a5db8193989b280
briannqc <brian.ngo313@gmail.com>
2022-02-10 00:52:53 +0800

    [8.1] Implementing web apps with a dynamic view
    Using @Controller, @RequestMapping, @PathVariable, @RequestParam
    and org.springframework.ui.Model with thymeleaf template engine.

--- ch8/src/main/kotlin/com/example/ch8/Ch8Application.kt
+++ ch8/src/main/kotlin/com/example/ch8/Ch8Application.kt
@@ -2,9 +2,29 @@ package com.example.ch8
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 
 @SpringBootApplication
 class Ch8Application
+
+@Controller
+class GreetingController {
+
+	@RequestMapping("/greeting/{username}")
+	fun greeting(
+		@PathVariable username: String,
+		@RequestParam color: String,
+		page: Model,
+	): String {
+		page.addAttribute("username", username)
+		page.addAttribute("color", color)
+		return "greeting.html"
+	}
+}
 
 fun main(args: Array<String>) {
 	runApplication<Ch8Application>(*args)
