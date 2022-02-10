5a032d89f546e057cabf2987608b0f066d85a61d
briannqc <brian.ngo313@gmail.com>
2022-02-11 01:27:08 +0800

    [8.2] Using GetMapping and PostMapping for GET and POST methods
    A toy product list using in-mem List, display with th:each="p: ${products}"
    POST with HTML form submit.

--- ch8/src/main/kotlin/com/example/ch8/Ch8Application.kt
+++ ch8/src/main/kotlin/com/example/ch8/Ch8Application.kt
@@ -3,10 +3,9 @@ package com.example.ch8
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.stereotype.Controller
+import org.springframework.stereotype.Service
 import org.springframework.ui.Model
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.*
 
 @SpringBootApplication
 class Ch8Application
@@ -25,6 +24,60 @@ class GreetingController {
 		return "greeting.html"
 	}
 }
+
+@Controller
+class ProductController(
+	private val productService: ProductService
+) {
+
+	@GetMapping("/products")
+	fun viewProducts(model: Model): String {
+		val products = productService.findAll()
+		model.addAttribute("products", products)
+		return "products.html"
+	}
+
+	@PostMapping("/products")
+	fun addProduct(
+		@RequestParam name: String,
+		@RequestParam price: Long,
+		model: Model,
+	): String {
+		val product = Product(name, price)
+		productService.add(product)
+
+		val products = productService.findAll()
+		model.addAttribute("products", products)
+		return "products.html"
+	}
+}
+
+interface ProductService {
+
+	fun findAll(): List<Product>
+
+	fun add(p: Product)
+}
+
+@Service
+class NonThreadSafeProductService : ProductService {
+
+	private val products: MutableList<Product> = mutableListOf()
+
+	override fun findAll(): List<Product> {
+		return products
+	}
+
+	override fun add(p: Product) {
+		products.add(p)
+
+	}
+}
+
+data class Product(
+	val name: String,
+	val price: Long,
+)
 
 fun main(args: Array<String>) {
 	runApplication<Ch8Application>(*args)
