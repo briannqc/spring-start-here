7a63e100f580865d8949506b69204895172dfee3
briannqc <brian.ngo313@gmail.com>
2022-03-25 01:16:04 +0700

    [Ch09] Using the application scope in a Spring web app
    Generally, we should avoid using application-scoped beans. Instead,
    we can consider using a persistence layer, such as a DB.

--- ch09/src/main/kotlin/com/example/ch09/Ch09Application.kt
+++ ch09/src/main/kotlin/com/example/ch09/Ch09Application.kt
@@ -9,8 +9,10 @@ import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.context.annotation.ApplicationScope
 import org.springframework.web.context.annotation.RequestScope
 import org.springframework.web.context.annotation.SessionScope
+import java.util.concurrent.atomic.AtomicInteger
 
 @SpringBootApplication
 class Ch09Application
@@ -51,7 +53,8 @@ class LoginController(
 
 @Controller
 class MainController(
-    private val loggedUserManagementService: LoggedUserManagementService
+    private val loggedUserManagementService: LoggedUserManagementService,
+    private val loginCountService: LoginCountService,
 ) {
 
     @GetMapping("/main")
@@ -60,15 +63,19 @@ class MainController(
         model: Model,
     ): String {
         println(loggedUserManagementService)
+        println(loginCountService)
+
         if (logout != null) {
             this.loggedUserManagementService.username = ""
         }
 
         val username = this.loggedUserManagementService.username
+        val loginCount = this.loginCountService.getCount()
         if (username == "") {
             return "redirect:/"
         }
         model.addAttribute("username", username)
+        model.addAttribute("loginCount", loginCount)
         return "welcome.html"
     }
 }
@@ -76,7 +83,8 @@ class MainController(
 @Component
 @RequestScope
 class LoginProcessor(
-    private val loggedUserManagementService: LoggedUserManagementService
+    private val loggedUserManagementService: LoggedUserManagementService,
+    private val loginCountService: LoginCountService,
 ) {
 
     lateinit var username: String
@@ -84,6 +92,8 @@ class LoginProcessor(
     lateinit var password: String
 
     fun login(): Boolean {
+        this.loginCountService.increase()
+
         val loggedIn = "nataline" == this.username && "password" == this.password
         if (loggedIn) {
             this.loggedUserManagementService.username = username
@@ -97,4 +107,19 @@ class LoginProcessor(
 class LoggedUserManagementService {
 
     var username: String = ""
+}
+
+@Service
+@ApplicationScope
+class LoginCountService {
+
+    private var count: AtomicInteger = AtomicInteger(0)
+
+    fun increase() {
+        this.count.incrementAndGet()
+    }
+
+    fun getCount(): Int {
+        return this.count.get()
+    }
 }
