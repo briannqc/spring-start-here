1fc2d33035e7abf7fe199bd6aca8bf09d9383fc6
briannqc <brian.ngo313@gmail.com>
2022-03-25 23:46:25 +0700

    [Ch10] @RestController = @Controller + @ResponseBody

--- ch10/src/main/kotlin/com/example/ch10/Ch10Application.kt
+++ ch10/src/main/kotlin/com/example/ch10/Ch10Application.kt
@@ -2,10 +2,33 @@ package com.example.ch10
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.ResponseBody
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
 class Ch10Application
 
 fun main(args: Array<String>) {
     runApplication<Ch10Application>(*args)
+}
+
+@Controller
+class HelloController {
+
+    @GetMapping("/v1/hello")
+    @ResponseBody
+    fun hello(): String {
+        return "Hello! with @Controller and @ResponseBody"
+    }
+}
+
+@RestController
+class HelloControllerV2 {
+
+    @GetMapping("/v2/hello")
+    fun hello(): String {
+        return "Hello v2 with @RestController!"
+    }
 }
