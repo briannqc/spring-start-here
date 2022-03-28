99526557ca3b84facf8f3d6f89eca64ababb06c6
briannqc <brian.ngo313@gmail.com>
2022-03-28 22:33:07 +0800

    [Ch11] Init the backend app

---
+++ ch11-app/src/main/kotlin/com/example/ch11app/Ch11AppApplication.kt
@@ -0,0 +1,11 @@
+package com.example.ch11app
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Ch11AppApplication
+
+fun main(args: Array<String>) {
+    runApplication<Ch11AppApplication>(*args)
+}
---
+++ ch11-app/src/test/kotlin/com/example/ch11app/Ch11AppApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.ch11app
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Ch11AppApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
