7eda10dcd0b01be556b94d961c20e7d8926bd948
briannqc <brian.ngo313@gmail.com>
2022-04-28 22:32:16 +0800

    [CH12] Init a fresh project

---
+++ ch12/src/main/kotlin/com/example/ch12/Ch12Application.kt
@@ -0,0 +1,11 @@
+package com.example.ch12
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Ch12Application
+
+fun main(args: Array<String>) {
+    runApplication<Ch12Application>(*args)
+}
