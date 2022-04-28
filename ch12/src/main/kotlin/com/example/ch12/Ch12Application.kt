e0609bf8b45929ba1a7a6eca02f2ddbca738712f
briannqc <brian.ngo313@gmail.com>
2022-04-29 00:18:37 +0800

    [CH12] Using a custom DataSource bean
    We generally use the Spring Boot-provided defaults,
    but specific cases sometimes need custom configurations
    or implementations for various optimizations.

--- ch12/src/main/kotlin/com/example/ch12/Ch12Application.kt
+++ ch12/src/main/kotlin/com/example/ch12/Ch12Application.kt
@@ -1,11 +1,39 @@
 package com.example.ch12
 
+import com.zaxxer.hikari.HikariDataSource
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import javax.sql.DataSource
 
 @SpringBootApplication
 class Ch12Application
 
 fun main(args: Array<String>) {
     runApplication<Ch12Application>(*args)
+}
+
+@Configuration
+class ProjectConfig(
+    @Value("\${custom.datasource.url}")
+    val datasourceUrl: String,
+
+    @Value("\${custom.datasource.username}")
+    val datasourceUsername: String,
+
+    @Value("\${custom.datasource.password}")
+    val datasourcePassword: String
+) {
+    @Bean
+    fun datasource(): DataSource {
+        val dataSource = HikariDataSource()
+        dataSource.jdbcUrl = datasourceUrl
+        dataSource.username = datasourceUsername
+        dataSource.password = datasourcePassword
+        dataSource.connectionTimeout = 1000
+
+        return dataSource
+    }
 }
