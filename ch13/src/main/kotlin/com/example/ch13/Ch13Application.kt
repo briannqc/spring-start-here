8c1b746b40b09c2eb298c50390db11649d5cdb8c
briannqc <brian.ngo313@gmail.com>
2022-04-29 14:30:30 +0800

    [CH13] Using transactions in Spring apps - Init project

---
+++ ch13/src/main/kotlin/com/example/ch13/Ch13Application.kt
@@ -0,0 +1,11 @@
+package com.example.ch13
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Ch13Application
+
+fun main(args: Array<String>) {
+    runApplication<Ch13Application>(*args)
+}
