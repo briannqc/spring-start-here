d78930726db13ac2bdeb565db9070d9de5aec737
briannqc <brian.ngo313@gmail.com>
2022-05-09 00:02:47 +0800

    [CH14] Implementing data persistence with Spring Data: Init project

---
+++ ch14/src/main/kotlin/com/example/ch14/Ch14Application.kt
@@ -0,0 +1,11 @@
+package com.example.ch14
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Ch14Application
+
+fun main(args: Array<String>) {
+    runApplication<Ch14Application>(*args)
+}
